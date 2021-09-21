a542734492cd49bfec7d18c3dbf27a2a73ba9d8e
Chris Pymm <chris@chrispymm.co.uk>
2021-09-21 12:39:40 +0100

    streamline ui, add recommended content hints

---
+++ src/migrations/2021_09_21_114756_update_metadata_description_fields.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateMetadataDescriptionFields extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('metadata', function(Blueprint $table) {
+            $table->text('description')->change();
+            $table->text('og_description')->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('metadata', function(Blueprint $table) {
+            $table->string('description')->change();
+            $table->string('og_description')->change();
+        });
+    }
+
+}
